d4a9be4c5207a534f34a2ec5c46339bd368f004d
Swakken <76128423+Swakken@users.noreply.github.com>
2024-02-20 16:33:01 +0100

    Update Highscores.java

--- memory/src/main/java/com/example/memory/Highscores.java
+++ memory/src/main/java/com/example/memory/Highscores.java
@@ -11,33 +11,34 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Highscores extends Application {
-    public void start(Stage mijnHomeStage) throws IOException {
-        //1 of meerdere Nodes aanmaken aanmaken
+    public void start(Stage mijnHighscoresStage) throws IOException {
+        // Nodes aanmaken
         Text txtTitel = new Text("Highscores");
-
+        Text score1 = new Text("1. Speler A - 100 punten");
+        Text score2 = new Text("2. Speler B - 90 punten");
+        Text score3 = new Text("3. Speler C - 80 punten");
+        Text score4 = new Text("4. Speler D - 70 punten");
+        Text score5 = new Text("5. Speler E - 60 punten");
 
         VBox buttonVBox = new VBox();
-        buttonVBox.getChildren().add(txtTitel);
-
+        buttonVBox.getChildren().addAll(txtTitel, score1, score2, score3, score4, score5);
 
         buttonVBox.setAlignment(Pos.CENTER);
         buttonVBox.setSpacing(12);
 
-
         // FullScreen
-        mijnHomeStage.setFullScreenExitHint("Highscores");
-        mijnHomeStage.setFullScreen(true);
+        mijnHighscoresStage.setFullScreenExitHint("Druk op ESC om te sluiten");
+        mijnHighscoresStage.setFullScreen(true);
 
-        //Scene toevoegen en pane daaraan toevoegen
+        // Scene toevoegen en Pane daaraan toevoegen
         //Je hebt normaal gezien maar 1 scene
         Scene sceneButtons = new Scene(buttonVBox);
-        //Scene toevoegen aan stage die je via parameter binnenkrijgt
-        mijnHomeStage.setScene(sceneButtons);
-        mijnHomeStage.show();
+        // Scene toevoegen aan stage
+        mijnHighscoresStage.setScene(sceneButtons);
+        mijnHighscoresStage.show();
     }
 
     public static void main(String[] args) {
         launch();
     }
 }
-
