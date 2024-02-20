716a9c4673b5a4d060b3729bf0c4f7ce1d3c0cf2
Swakken <76128423+Swakken@users.noreply.github.com>
2024-02-20 09:59:59 +0100

    push

---
+++ memory/src/main/java/com/example/memory/Home.java
@@ -0,0 +1,62 @@
+package com.example.memory;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Home extends Application {
+
+    public void start(Stage mijnStage) throws IOException {
+        //1 of meerdere Nodes aanmaken aanmaken
+        Text txtTitel = new Text("Memory Spel");
+        Button btnStartSpel = new Button("Start Spel");
+        Button btnHighscores = new Button("Highscores");
+        Button btnHandleiding = new Button("Handleiding");
+        Button btnInstellingen = new Button("Instellingen");
+        Button btnAfsluiten = new Button("Afsluiten");
+
+        btnStartSpel.setPrefSize(200,100);
+        btnHighscores.setPrefSize(200,100);
+        btnHandleiding.setPrefSize(200,100);
+        btnInstellingen.setPrefSize(200,100);
+        btnAfsluiten.setPrefSize(200,100);
+
+        VBox buttonVBox = new VBox();
+        buttonVBox.getChildren().add(txtTitel);
+        buttonVBox.getChildren().add(btnStartSpel);
+        buttonVBox.getChildren().add(btnHighscores);
+        buttonVBox.getChildren().add(btnHandleiding);
+        buttonVBox.getChildren().add(btnInstellingen);
+        buttonVBox.getChildren().add(btnAfsluiten);
+
+        buttonVBox.setAlignment(Pos.CENTER);
+        buttonVBox.setSpacing(12);
+
+
+
+
+
+
+        // FullScreen
+//        mijnStage.setFullScreen(true);
+
+        //Scene toevoegen en pane daaraan toevoegen
+        //Je hebt normaal gezien maar 1 scene
+        Scene sceneButtons = new Scene(buttonVBox);
+        //Scene toevoegen aan stage die je via parameter binnenkrijgt
+        mijnStage.setScene(sceneButtons);
+        mijnStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
