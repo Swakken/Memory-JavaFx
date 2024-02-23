691dacfbb008fa5c9180d4c67ae8258c1e1cd6d1
FredericBackeljau <frederic.backeljau@student.kdg.be>
2024-02-23 16:01:39 +0100

    Handleiding view aangemaakt

--- memory/src/main/java/com/example/memory/Spelscherm.java
+++ memory/src/main/java/com/example/memory/Spelscherm.java
@@ -2,6 +2,10 @@ package com.example.memory;
 
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -9,6 +13,18 @@ import java.io.IOException;
 public class Spelscherm extends Application {
 
     public void start(Stage mijnSpelschermStage) throws IOException {
+        //Nodes aanmaken
+        GridPane gpSpelScherm = new GridPane();
+
+
+        //Pane op een Scene zetten
+        Scene SpelScene = new Scene(gpSpelScherm);
+
+        mijnSpelschermStage.setScene(SpelScene);
+        mijnSpelschermStage.show();
+
+
+
 
     }
 
---
+++ memory/src/main/java/com/example/memory/view/HandleidingView.java
@@ -0,0 +1,61 @@
+package com.example.memory.view;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+/**
+ * Frederic Backeljau
+ * 2/23/2024
+ */
+public class HandleidingView extends VBox {
+    private Text txtTitel;
+    private Button terugButton;
+    private Text intro;
+    private Text spelregelsTitel;
+    private Text spelregels;
+    private Text bedieningTitel;
+    private Text bediening;
+
+
+public HandleidingView() {
+    initialiseNodes();
+    layoutNodes();
+}
+
+private void initialiseNodes(){
+    txtTitel = new Text("Memory Spel Handleiding");
+    terugButton = new Button("Terug naar Home");
+    intro = new Text("Welkom bij het Memory Spel!");
+    spelregelsTitel = new Text("Spelregels:");
+    spelregels = new Text(
+            "1. Alle kaarten liggen omgekeerd.\n" +
+                    "2. Elke beurt mag je twee kaarten omdraaien.\n" +
+                    "3. Als de kaarten overeenkomen, blijven ze omgedraaid.\n" +
+                    "4. Het doel is om alle paren te vinden binnen de opgegeven tijd.\n" +
+                    "5. Het spel eindigt als alle kaarten zijn omgedraaid of als de tijd verstreken is."
+    );
+    bedieningTitel = new Text("Bediening:");
+    bediening = new Text(
+            "- Gebruik de muis om kaarten te selecteren.\n" +
+                    "- ESC toets om het spel te pauzeren."
+    );
+
+
+}
+
+private void layoutNodes(){
+    txtTitel.setFont(Font.font("Arial", 24));
+    terugButton.setFont(Font.font("Arial", 14));
+    terugButton.setStyle("-fx-background-color: #E76F51; -fx-text-fill: white;");
+    this.getChildren().addAll(txtTitel, intro, spelregelsTitel, spelregels, bedieningTitel, bediening, terugButton);
+    this.setAlignment(Pos.CENTER);
+    this.setSpacing(10);
+
+
+
+}
+}
