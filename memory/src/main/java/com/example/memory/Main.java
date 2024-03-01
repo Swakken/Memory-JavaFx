49bde34574e2a007e27f6329f1eb281b88bcc209
Swakken <76128423+Swakken@users.noreply.github.com>
2024-03-01 16:01:57 +0100

    icon

--- memory/src/main/java/com/example/memory/Main.java
+++ memory/src/main/java/com/example/memory/Main.java
@@ -4,6 +4,7 @@ import com.example.memory.Schermen.home.HomePresenter;
 import com.example.memory.Schermen.home.HomeView;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,6 +14,16 @@ public class Main extends Application {
 
         HomeView view = new HomeView();
         HomePresenter presenter = new HomePresenter(view);
+//        primaryStage.getIcons().add(new Image(this.getClass().getResourceAsStream("/icon.png")));
+
+
+        primaryStage.getIcons().add(new Image("https://i.stack.imgur.com/6ZjZM.png"));
+
+
+//        primaryStage.getIcons().add(
+//                new Image(Main.class.getResourceAsStream( "/icon.png" )));
+
+        //   primaryStage.getIcons().add(new Image("file:icon.png"));
 
         Scene scene = new Scene(view);
         primaryStage.setScene(scene);
