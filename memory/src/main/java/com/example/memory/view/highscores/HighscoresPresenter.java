ad4943d0f78eb0c86cef58505af444266d365789
Swakken <76128423+Swakken@users.noreply.github.com>
2024-03-01 11:17:42 +0100

    Update HighscoresPresenter.java

--- memory/src/main/java/com/example/memory/view/highscores/HighscoresPresenter.java
+++ memory/src/main/java/com/example/memory/view/highscores/HighscoresPresenter.java
@@ -1,6 +1,10 @@
 package com.example.memory.view.highscores;
 
+import com.example.memory.view.home.HomePresenter;
 import com.example.memory.view.home.HomeView;
+import com.example.memory.view.startspel.StartSpelPresenter;
+import com.example.memory.view.startspel.StartSpelView;
+import javafx.event.ActionEvent;
 
 public class HighscoresPresenter {
 
@@ -8,5 +12,17 @@ public class HighscoresPresenter {
 
     public HighscoresPresenter(HighscoresView view) {
         this.view = view;
+        this.addEventListnersHomePagina();
     }
+
+    private void addEventListnersHomePagina() {
+        this.view.getTerugButton().addEventHandler(ActionEvent.ACTION, (actionEvent) -> {
+
+            HomeView mijnHomeView = new HomeView();
+            HomePresenter mijnHomePresenter = new HomePresenter(mijnHomeView);
+
+            this.view.getScene().setRoot(mijnHomeView);
+        });
+    }
+
 }
