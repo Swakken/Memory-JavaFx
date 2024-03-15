690226c3b1ef3dcdedaeb83c4a6454ee35b319e5
Swakken <76128423+Swakken@users.noreply.github.com>
2024-03-15 13:23:42 +0100

    alert bij afsluiten

--- memory/src/main/java/com/example/memory/base/BasePresenter.java
+++ memory/src/main/java/com/example/memory/base/BasePresenter.java
@@ -2,6 +2,11 @@ package com.example.memory.base;
 
 import com.example.memory.base.BaseView;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
 
 public abstract class BasePresenter<ViewType extends BaseView> {
     protected ViewType view;
@@ -17,8 +22,25 @@ public abstract class BasePresenter<ViewType extends BaseView> {
     }
 
     private void addEventListenersAfsluiten() {
-        this.view.getAfsluiten().addEventHandler(ActionEvent.ACTION, actionEvent -> System.exit(0));
+        this.view.getAfsluiten().addEventHandler(ActionEvent.ACTION, actionEvent -> {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Afsluiten");
+            alert.setHeaderText("Weet je zeker dat je dit scherm wilt afsluiten?");
+
+            ButtonType jaButton = new ButtonType("Ja");
+            ButtonType cancelButton = new ButtonType("Annuleren", ButtonBar.ButtonData.CANCEL_CLOSE);
+            alert.getButtonTypes().setAll(jaButton, cancelButton);
+
+            alert.getDialogPane().getStylesheets().add(getClass().getResource("/styleheets/alert.css").toExternalForm());
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == jaButton) {
+                // Als de gebruiker op "Ja" klikt, sluit het scherm af
+                System.exit(0);
+            }
+        });
     }
+
 
 //    private void addEventListenersInstellingen() {
 //        this.view.getInstellingen().addEventHandler(ActionEvent.ACTION, (actionEvent) -> {
